1aec81a4fa926423e9237bf962c52406fc58619e
Doctor-Paradox <78156703+Doctor-Paradox@users.noreply.github.com>
2021-10-27 00:19:50 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,17 +1,8 @@
 set -e
 sudo apt install figlet
 [ -d ~/bin ] || mkdir ~/bin
-cp bkg_multicircle_xmm.py ~/bin/bkg_multicircle_xmm.py
-cp coord.py ~/bin/coord.py
-cp coordX.py ~/bin/coordX.py
-cp coordY.py ~/bin/coordY.py
-cp src_circle.py ~/bin/src_circle.py
-cp for_phasecalc.py ~/bin/for_phasecalc.py
-cp rgs_contaminator.py ~/bin/rgs_contaminator.py
-cp rgs_pileup.py ~/bin/rgs_pileup.py
-cp rgs_user_index_finder.py ~/bin/rgs_user_index_finder.py
-cp time_fil_expre_maker.py ~/bin/time_fil_expre_maker.py
-cp xmm_newton_advanced_script.sh ~/bin/xmm_newton_advanced_script.sh
+cp *.py ~/bin/
+cp *.sh ~/bin/
 
 chmod 700 ~/bin/xmm_newton_advanced_script.sh
 (echo "#----------------XMM SCRIPT---------------------")>>~/.bashrc
